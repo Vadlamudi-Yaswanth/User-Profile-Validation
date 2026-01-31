60bd88aee500e5659e43d5bc2ccbff4b494683df
Vadlamudi-Yaswanth <vadlamudiyaswanthu@gmail.com>
2026-01-31 18:46:52 +0530

    Refactor student approval logic with registration number

--- Day2/Day2_Challenge.py
+++ Day2/Day2_Challenge.py
@@ -1,10 +1,11 @@
+reg_no="AP24110011607"
+last_digit=int(reg_no[-1])
 Student=input("Enter Student ID: ")
 digit_found=False
 student_valid=False
 if len(Student) == 7:
     if Student[4].isdigit() and Student[5].isdigit() and Student[6].isdigit():
         digit_found = True
-
     if Student[0]=="C" and Student[1]=="S" and Student[2]=="E" and Student[3]=="-" and digit_found:
         student_valid = True
 Email=input("Enter Email Id: ")
@@ -20,18 +21,19 @@ if len(password) >= 8:
     if password[0]>= 'A' and password[0]<= 'Z':
         if any(ch.isdigit() for ch in password):
             valid_password = True
-
 Referral=input("Enter Referral: ")
 valid_Referral=False
 valid_start=False
 if len(Referral) >= 6:
     if Referral[0]=="R" and Referral[1]=="E" and Referral[2]=="F":
         valid_start = True
-
     if valid_start and Referral[3].isdigit() and Referral[4].isdigit() and Referral[-1]=="@":
         valid_Referral = True
-
-if student_valid and valid_Email and valid_password and valid_Referral:
+if last_digit%2 !=0:
+    approved=valid_password and student_valid
+else:
+    approved=student_valid and valid_password
+if approved and valid_Email and valid_Referral:
     print("Approved")
 else:
     print("Rejected")
