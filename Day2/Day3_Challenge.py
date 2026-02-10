5cd72b4ea4dda68a5974aa5fe2c83c3b4265377f
Vadlamudi-Yaswanth <vadlamudiyaswanthu@gmail.com>
2026-02-10 14:34:53 +0530

    Add files via upload

---
+++ Day2/Day3_Challenge.py
@@ -0,0 +1,50 @@
+n=int(input("Enter the no.of students marks to be entered:"))
+marks=[0]*n
+for i in range(n):
+    marks[i]=int(input(f"Enter the mark of  student {i+1}:"))
+reg_no=input("Enter the registration number:")
+name=input("Enter your name:")
+section=input("Enter the section:")
+count1=0
+count2=0
+if reg_no[-1]=="2" or reg_no[-1]=="3" or reg_no[-1]=="5" or reg_no[-1]=="7" or reg_no[-1]=="9":
+    for i in range(len(marks)):
+        marks[i]+=5
+else:
+    for i in range(len(marks)):
+        marks[i]+=2
+if len(name)>=8:
+    for i in range(len(marks)):
+        marks[i]+=8
+else:
+    for i in range(len(marks)):
+        marks[i]+=6
+if section=="A" or section=="E" or section=="I" or section=="O" or section=="U":
+    for i in range(len(marks)):
+        marks[i]+=5
+else:
+    for i in range(len(marks)):
+        marks[i]+=2
+print(marks)
+for mark in marks:
+    if mark>=90 and mark<=100:
+        count1+=1
+        print("Excellent")
+    elif mark>=75 and mark<=89:
+        count1+=1
+        print("Very Good")
+    elif mark>=60 and mark<=74:
+        count1+=1
+        print("Good")
+    elif mark>=40 and mark<=59:
+        count1+=1
+        print("Average")
+    elif mark>=0 and mark<=39:
+        count2+=1
+        print("Fail")
+    else:
+        print("Invalid Entry")
+print("Total no.of valid students is:",count1)
+print("Total no.of Invalid students is:",count2)
+
+
