d41046cd1fabac6590015a43ed2dcb8d333a5837
Vadlamudi-Yaswanth <vadlamudiyaswanthu@gmail.com>
2026-02-17 18:37:54 +0530

    Refactor request handling with PLI logic
    Added logic to remove requests based on PLI value.

--- Day2/Day5_Challenge.py
+++ Day2/Day5_Challenge.py
@@ -22,7 +22,39 @@ for i in request_values:
         total_valid+=1
     else:
         total_valid+=1
-
 L=17
 print("Length of my full name is:",L)
 PLI=L%3
+if PLI==0:
+    removed_count=0
+    while len(low_demand)>0:
+        low_demand.pop()
+        removed_count+=1
+elif PLI==1:
+    removed_count=0
+    while len(high_demand) > 0:
+        high_demand.pop()
+        removed_count += 1
+else:
+    removed_count = 0
+    while len(low_demand) > 0:
+        low_demand.pop()
+        removed_count += 1
+    while len(high_demand) > 0:
+        high_demand.pop()
+        removed_count += 1
+    while len(invalid_request) > 0:
+        invalid_request.pop()
+        removed_count += 1
+
+print("PLI is:",PLI)
+print("Total valid requests:",total_valid)
+print("Removed requests:",removed_count)
+print("Lowed demand:",low_demand)
+print("High demand:",high_demand)
+print("Moderate demand:",moderate_demand)
+print("Invalid requests:",invalid_request)
+
+
+
+
