3d09b885aa9d2bab4da647c6ecf170c55f65c791
Vadlamudi-Yaswanth <vadlamudiyaswanthu@gmail.com>
2026-02-17 18:36:16 +0530

    Refactor request input and categorize demand
    Refactored request handling logic and added validation for demand categories.

--- Day2/Day5_Challenge.py
+++ Day2/Day5_Challenge.py
@@ -8,3 +8,21 @@ high_demand=[]
 moderate_demand=[]
 invalid_request=[]
 total_valid=0
+for i in request_values:
+    if i<0:
+        invalid_request.append(i)
+    elif i>=1 and i<=20:
+        low_demand.append(i)
+        total_valid+=1
+    elif i>=21 and i<=50:
+        moderate_demand.append(i)
+        total_valid+=1
+    elif i>50:
+        high_demand.append(i)
+        total_valid+=1
+    else:
+        total_valid+=1
+
+L=17
+print("Length of my full name is:",L)
+PLI=L%3
