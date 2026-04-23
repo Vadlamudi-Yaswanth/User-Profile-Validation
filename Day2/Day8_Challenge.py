6672bccdab5a89dcf687f56d7d2d8dd074d1a135
Vadlamudi-Yaswanth <vadlamudiyaswanthu@gmail.com>
2026-04-23 22:26:08 +0530

    Update Day8_Challenge.py

--- Day2/Day8_Challenge.py
+++ Day2/Day8_Challenge.py
@@ -2,4 +2,15 @@ import random
 import pandas as pd
 import numpy as np
 import math
+import matplotlib.pyplot as plt
+def create_student_data(n):
+    records = []
+    for i in range(n):
+        sid = f"S{i+1:03d}"
+        m = random.randint(0, 100)
+        att = random.randint(0, 100)
+        assign = random.randint(0, 50)
+        perf_score = math.sqrt(m * assign) + (2 * att) - math.log(m + 1)
 
+        records.append((sid, m, att, assign, perf_score))
+    return records
